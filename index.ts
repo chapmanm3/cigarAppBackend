b60dcc15d4dc8ba71cb2ad34af0a49f7dd9cd8e4
Matt Chapman <chapmancac@gmail.com>
2025-01-26 12:36:50 -0500

    quick commit 2

--- index.ts
+++ index.ts
@@ -2,6 +2,12 @@ import { addCigarHandler, getAllCigarsHandler } from "./src/handlers/cigars";
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors'
+import { initializeApp, applicationDefault } from "firebase-admin/app";
+
+//Init firebase admin
+const fireBase = initializeApp({
+  credential: applicationDefault()
+})
 
 const app = express()
 const port = 3000
